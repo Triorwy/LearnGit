6d2de6bec733845c1905da58531ac29d9a69a150
liuzhao.liu <liuzhao.liu@qunar.com>
2019-08-24 16:19:14 +0800

    'add'

--- Test/src/java/service/Test.java
+++ Test/src/java/service/Test.java
@@ -7,5 +7,6 @@ package java.service;
 public class Test {
     public static void main(String[] args) {
         System.out.println("change world!");
+        System.out.println("Just do it!");
     }
 }
