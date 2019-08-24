577d5a500d2dd8d6cc20aa4f7cc00b674457760b
liuzhao.liu <liuzhao.liu@qunar.com>
2019-08-24 16:16:57 +0800

    'test'

---
+++ Test/src/java/service/Test.java
@@ -0,0 +1,11 @@
+package java.service;
+
+/**
+ * @author liuzhao.liu
+ * @date 2019/8/24 16:07 mobile phone：132-0145-6372
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("change world!");
+    }
+}
