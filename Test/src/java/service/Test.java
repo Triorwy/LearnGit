ee77201207745cfd1eb8ad404395983dd4b80305
liuzhao.liu <liuzhao.liu@qunar.com>
2019-08-24 16:34:49 +0800

    'asd'

--- Test/src/java/service/Test.java
+++ Test/src/java/service/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("change world!");
         System.out.println("Just do it!");
+        System.out.println("ahhhhhhhhhhhh");
     }
 }
